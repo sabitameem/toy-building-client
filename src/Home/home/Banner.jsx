23661e1787cb7e94be0bfb008bba5f6eb69f8309
sabitameem <sabitameem578@gmail.com>
2023-05-18 22:07:24 +0530

    gallery section added

--- src/Home/home/Banner.jsx
+++ src/Home/home/Banner.jsx
@@ -3,7 +3,15 @@ import Lottie from 'lottie-react'
 const Banner = () => {
     return (
         <div className='bg-gradient-to-r from-purple-400  to-red-400'>
+
+            
             <div className='my-container flex flex-col items-center justify-between lg:flex-row'>
+                {/* lottie animation */}
+            <div className='relative lg:w-1/2 '>
+                 <div className='w-full lg:w-4/5 lg:ml-auto'>
+                     <Lottie animationData={toy} loop={true}></Lottie>
+                 </div>
+            </div>
             <div  className="lg:w-1/2 mb-10 lg:mb-0 lg:mr-10" >
             
             <h1 className="text-5xl font-bold text-white mb-6">
@@ -14,12 +22,7 @@ const Banner = () => {
             Experience the <span className="text-yellow-300">Ultimate</span> online toy shopping brought to you by <span className="text-yellow-300">Toy Building</span>. Let your imagination run wild and discover a world of fun and excitement!
           </p>
             </div>
-            {/* lottie animation */}
-            <div className='relative lg:w-1/2 '>
-                 <div className='w-full lg:w-4/5 lg:ml-auto'>
-                     <Lottie animationData={toy} loop={true}></Lottie>
-                 </div>
-            </div>
+            
         </div>
         </div>
     );
--- src/Home/home/Gallery.jsx
+++ src/Home/home/Gallery.jsx
@@ -1,11 +1,100 @@
+// import React from 'react';
+
+// const Gallery = () => {
+//   const images = [
+//     'https://i.ibb.co/NF35gnR/633aad0dcb0629168c563ca3-toy-cars-sian-fkp3-metal-model-car-with.jpg',
+//     'https://i.ibb.co/TcgTNBX/cool-toy-cars-for-kids-to-drive.jpg',
+//     'https://i.ibb.co/wdmTh9V/spraycarvideo-mp4.jpg',
+//     'https://i.ibb.co/TmyW7rD/Wholesale-Facoty-Supplier-Electric-Remote-Control-Toy-Car-Drift-RC-Car-Spray-Cars-Toy-with-Cool-LED.jpg',
+//     'https://i.ibb.co/jJSrJJS/2-In-1-Transformation-Car-Toy-Trnasformational-Robot-Model-ABS-Mini-Cool-Pull-Back-Toy-Car-jpg-Q90-j.webp',
+//     'https://i.ibb.co/VWSfmvq/Toy-Cars-Ultimate-Grid-4.jpg',
+//     'https://i.ibb.co/rv1ztVN/SPR-HOME-6-best-toy-cars-for-kids-4155717-01-b04a22807dd14b10a647f7d769a86224.jpg',
+//     'https://i.ibb.co/Qcbv9dk/images-q-tbn-ANd9-Gc-TGh-Wrw-Hk-V7sz-IVw-Ui-Rvdcz60-F8-Rs-Vro-L7-XXw-usqp-CAU.jpg',
+//     'https://i.ibb.co/JqmG31D/wolvolk-2be7f3f6a9134724900f1c703aaf3dd6.jpg'
+    
+//     // Add more image URLs here
+//   ];
+
+
+  
+
+//   return (
+//     <div className="container mx-auto p-4">
+//       <h2 className="text-3xl font-bold mb-4">Toys Collections</h2>
+//       <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
+//         {images.map((image, index) => (
+//           <div key={index} className="relative group">
+//             <img src={image} alt={`Image ${index + 1}`} className="rounded-lg shadow-md w-80 h-52" />
+            
+//           </div>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Gallery;
+
+
+
+
+
+
+
+
+
+
+import React, { useState } from 'react';
 
 
 const Gallery = () => {
-    return (
-        <div>
-            Gallery
-        </div>
-    );
+  const images = [
+    'https://i.ibb.co/NF35gnR/633aad0dcb0629168c563ca3-toy-cars-sian-fkp3-metal-model-car-with.jpg',
+    'https://i.ibb.co/TcgTNBX/cool-toy-cars-for-kids-to-drive.jpg',
+    'https://i.ibb.co/wdmTh9V/spraycarvideo-mp4.jpg',
+    'https://i.ibb.co/TmyW7rD/Wholesale-Facoty-Supplier-Electric-Remote-Control-Toy-Car-Drift-RC-Car-Spray-Cars-Toy-with-Cool-LED.jpg',
+    'https://i.ibb.co/jJSrJJS/2-In-1-Transformation-Car-Toy-Trnasformational-Robot-Model-ABS-Mini-Cool-Pull-Back-Toy-Car-jpg-Q90-j.webp',
+    'https://i.ibb.co/VWSfmvq/Toy-Cars-Ultimate-Grid-4.jpg',
+    'https://i.ibb.co/rv1ztVN/SPR-HOME-6-best-toy-cars-for-kids-4155717-01-b04a22807dd14b10a647f7d769a86224.jpg',
+    'https://i.ibb.co/Qcbv9dk/images-q-tbn-ANd9-Gc-TGh-Wrw-Hk-V7sz-IVw-Ui-Rvdcz60-F8-Rs-Vro-L7-XXw-usqp-CAU.jpg',
+    'https://i.ibb.co/JqmG31D/wolvolk-2be7f3f6a9134724900f1c703aaf3dd6.jpg'
+  ];
+
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const handlePrev = () => {
+    setActiveIndex((prevIndex) => (prevIndex === 0 ? images.length - 1 : prevIndex - 1));
+  };
+
+  const handleNext = () => {
+    setActiveIndex((prevIndex) => (prevIndex === images.length - 1 ? 0 : prevIndex + 1));
+  };
+
+  return (
+    <div className="container mx-auto p-4">
+      <h2 className="text-4xl lg:text-6xl font-bold mb-4 text-pink-400 text-center">Toys Collections!!!! <span className='text-yellow-300'>Yaaahhhhh!</span></h2>
+      <p className='text-xl font-semibold text-blue-400 text-center mt-0'>Let's explore.......</p>
+      <div className="relative">
+        <button
+          className="absolute top-1/2 left-0 transform -translate-y-1/2 bg-black bg-opacity-25 hover:bg-opacity-50 text-black rounded-full w-10 h-10 flex items-center justify-center"
+          onClick={handlePrev}
+        >
+          &lt;
+        </button>
+        <img
+          src={images[activeIndex]}
+          alt={`Image ${activeIndex + 1}`}
+          className="w-80 h-52 rounded-lg shadow-md lg:w-full lg:h-96 mx-auto sm:max-w-xs md:max-w-sm lg:max-w-md"
+        />
+        <button
+          className="absolute top-1/2 right-0 transform -translate-y-1/2 bg-black bg-opacity-25 hover:bg-opacity-50 text-black rounded-full w-10 h-10 flex items-center justify-center"
+          onClick={handleNext}
+        >
+          &gt;
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Gallery;
