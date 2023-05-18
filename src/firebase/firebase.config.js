78b5ac60ea08e8eb6324525ba2d908ed129b7eb1
sabitameem <sabitameem578@gmail.com>
2023-05-18 17:44:48 +0530

    firebase keys

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,7 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-//   VITE_apiKey=AIzaSyBar1xfK0kYpb8S3u53Xop_BjCADskFCHU
-// VITE_authDomain=toy-building-project.firebaseapp.com
-// VITE_projectId=toy-building-project
-// VITE_storageBucket=toy-building-project.appspot.com
-// VITE_messagingSenderId=320957466870
-// VITE_appId=1:320957466870:web:13f7b43974ad788a2584d7
+
   apiKey: import.meta.env.VITE_apiKey,
   authDomain: import.meta.env.VITE_authDomain,
   projectId: import.meta.env.VITE_projectId,
