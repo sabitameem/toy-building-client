8bb6590f2b8022206dddd9716479a12d6a88d18c
sabitameem <sabitameem578@gmail.com>
2023-05-21 15:52:31 +0530

    search field added

--- src/pages/AllToys.jsx
+++ src/pages/AllToys.jsx
@@ -3,6 +3,8 @@ import AllToysRow from "./AllToysRow";
 
 const AllToys = () => {
   const [toys, setToys] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [filteredToys, setFilteredToys] = useState([]);
 
   useEffect(() => {
     fetchToys();
@@ -15,20 +17,42 @@ const AllToys = () => {
       );
       const data = await response.json();
       setToys(data);
+      setFilteredToys(data);
     } catch (error) {
       console.log(error);
     }
   };
 
+  useEffect(() => {
+    if (searchQuery) {
+      const filteredData = toys.filter((toy) =>
+        toy.name.toLowerCase().includes(searchQuery.toLowerCase())
+      );
+      setFilteredToys(filteredData);
+    } else {
+      setFilteredToys(toys);
+    }
+  }, [toys, searchQuery]);
+
   return (
     <div className="">
+      <div className="lg:flex justify-center">
+      <h4 className="text-xl lg:text-4xl text-center font-semibold text-pink-400">All Of Toys are here</h4>
+      <input
+        type="text"
+        placeholder="Search toys"
+        value={searchQuery}
+        className="input w-full input-sm lg:input-md input-bordered max-w-xs mb-3 mt-2 ms-12"
+        onChange={(e) => setSearchQuery(e.target.value)}
+      />
+      </div>
       
+
       <div className="overflow-x-auto w-full">
         <table className="table w-full">
           {/* head */}
           <thead>
             <tr>
-              
               <th>Seller</th>
               <th>Name</th>
               <th>Sub-Catergory</th>
@@ -38,10 +62,13 @@ const AllToys = () => {
             </tr>
           </thead>
           <tbody>
-            {toys.map((toy) => <AllToysRow
-            key={toy._id}
-            toy={toy}
-            ></AllToysRow> )}
+            {filteredToys.slice(0, 20).map((toy) => (
+              <AllToysRow key={toy._id} toy={toy} />
+            ))}
+
+            {/* {toys.map((toy) => (
+              <AllToysRow key={toy._id} toy={toy}></AllToysRow>
+            ))} */}
           </tbody>
         </table>
       </div>
